7ca45a6d5a2920fddce706f74487cc0eab15ffea
Mark Fu <marktfu08@gmail.com>
2026-03-06 20:12:58 -0500

    Modification: Work in progress, add graphing for angular displacement of stepper motor as a function of time

--- ContStepMot.py
+++ ContStepMot.py
@@ -1,12 +1,17 @@
 #Stepmotor, have motor pins be on GPIO 17,18,19,20.
+#WIP: Angular Displacement as function of time
 import RPi.GPIO as GPIO
 from RpiMotorLib.RpiMotorLib import BYJMotor
+from matplotlib import pyplot as py
 
 GPIO.setmode(GPIO.BCM)
 
 pins = [17,18,19,20]
 GPIO.setup(pins,GPIO.OUT)
 motor = BYJMotor("stepper", "28BYJ48")
+x = []
+y = []
+displacement = 0
 
 def get_valid_input(prompt, value_type=float):
     while True:
@@ -29,6 +34,15 @@ def loop():
         is_ccw = (direction == 1)
 
         print(f"Running: {rev} revolutions, {speed} rad/s, CCW={is_ccw}")
+
+        rad_per_step = (2*math.pi)/512
+        step_value = rad_per_step if is_ccw else -rad_per_step
+        
+        for i in range(int(steps)):
+            x.append(i * delay)
+            current_rad += step_value
+            y.append(current_rad)
+            
         
         # Execute movement
         motor.motor_run(pins, delay, steps, is_ccw, False, "half", 0.05)
@@ -37,6 +51,9 @@ if __name__ == "__main__":
     try:
         loop()
     except KeyboardInterrupt:
+        py.plot(x,y)
+        plt.title("Angular Displacement as a function of time")
+        plt.show()
         motor.motor_stop()
         GPIO.output(pins, GPIO.LOW)
         GPIO.cleanup()
