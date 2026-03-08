d980862caf7380345faa15a528b440a28a96d179
Mark Fu <marktfu08@gmail.com>
2026-03-07 21:41:24 -0500

    Modification: Added threading behavior for graphing, though additional tests needed to confirm applicability of input to motor functions

--- ContStepMot.py
+++ ContStepMot.py
@@ -1,17 +1,29 @@
 #Stepmotor, have motor pins be on GPIO 17,18,19,20.
-#WIP: Angular Displacement as function of time
 import RPi.GPIO as GPIO
 from RpiMotorLib.RpiMotorLib import BYJMotor
 from matplotlib import pyplot as py
+import math
+import threading
+import time
 
 GPIO.setmode(GPIO.BCM)
 
-pins = [17,18,19,20]
+pins = [17,18,27,22]
 GPIO.setup(pins,GPIO.OUT)
 motor = BYJMotor("stepper", "28BYJ48")
 x = []
 y = []
-displacement = 0
+displacement = 0.0
+start_time = time.time()
+running = True
+
+def bglogging():
+    global displacement, running
+    while running:
+        elapsed = time.time()-start_time
+        x.append(elapsed)
+        y.append(displacement)
+        time.sleep(0.1)
 
 def get_valid_input(prompt, value_type=float):
     while True:
@@ -21,12 +33,13 @@ def get_valid_input(prompt, value_type=float):
             print(f"Invalid input. Please enter a {value_type.__name__}.")
 
 def loop():
+    global displacement
     while True:
         rev = get_valid_input("Enter number of revolutions: ")
         steps = rev * 512
         speed = get_valid_input("Enter angular speed in radians per seconds (max 1.5): ", float)
         #need to correct speed conversion
-        delay = (2*math.pi)/(512*speed)
+        delay = (2* math.pi)/(512*speed)
         direction = -1
         while direction not in [0, 1]:
             direction = get_valid_input("Enter direction (0 for CW, 1 for CCW): ", int)
@@ -35,13 +48,10 @@ def loop():
 
         print(f"Running: {rev} revolutions, {speed} rad/s, CCW={is_ccw}")
 
-        rad_per_step = (2*math.pi)/512
+        rad_per_step = (2*math.pi)/512*speed
         step_value = rad_per_step if is_ccw else -rad_per_step
         
-        for i in range(int(steps)):
-            x.append(i * delay)
-            current_rad += step_value
-            y.append(current_rad)
+        displacement += (step_value * steps)
             
         
         # Execute movement
@@ -49,11 +59,17 @@ def loop():
 
 if __name__ == "__main__":
     try:
+        log_thread = threading.Thread(target=bglogging, daemon=True)
+        log_thread.start()
         loop()
     except KeyboardInterrupt:
+        running = False
+        plt.figure(figsize=(10,5))
         py.plot(x,y)
         plt.title("Angular Displacement as a function of time")
+        plt.xlabel("Time (seconds)")
+        plt.ylabel("Displacemene (radians)")
+        plt.grid(True)
         plt.show()
-        motor.motor_stop()
         GPIO.output(pins, GPIO.LOW)
         GPIO.cleanup()
